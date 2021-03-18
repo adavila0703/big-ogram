fe4b4f19f84724635a23582da829398cc3e41bf1
adavila0703 <36370758+adavila0703@users.noreply.github.com>
2021-03-18 06:34:28 -0500

    Change bigogram py file name

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ nums_list = [x for x in range(10000)]
 def in_num_list(test):
     ret = []
     for i in range(0, 100):
-        ret.append(test * i)
+        ret.append(test in nums_list)
     return ret
 
 test.calc_bigo(func = in_num_list, loop = 1000)
